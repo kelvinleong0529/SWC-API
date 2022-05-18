1b7f08c692bf6807ca067b2a062701ad0dff5fdb
Kelvin Leong Zhin Sheng <zhinsheng97@hotmail.com>
2022-05-18 17:06:27 +0800

    Adding Express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello World!")
+import express from 'express'
+import morgan from 'morgan';
+
+const app = express();
+app.use(morgan('dev'))
+
+app.get('/', (req, res) => {
+    res.json({ hello: 'world!' })
+})
+
+// everything that comes through environment variable is a string
+// we need to convert the environment variable to a number
+const port: number = Number(process.env.PORT ?? 8080);
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server started at http://localhost:${port}`)
+})
+// in docker when we are attaching to a port we need to listen at '0.0.0.0'
